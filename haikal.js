416a499d8962744ee08ace7913eeef56dff1a2cf
Fahrul6868 <117023435+Fahrul6868@users.noreply.github.com>
2023-08-09 20:35:37 +0700

    Update haikal.js

--- haikal.js
+++ haikal.js
@@ -60,7 +60,7 @@ var prefix = prefa ? /^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi.t
 
 //=================================================//
 const isCmd = body.startsWith(prefix)
-const command = body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase()//Kalau mau Single prefix Lu ganti pake ini = const command = body.slice(1).trim().split(/ +/).shift().toLowerCase()
+const command = body.slice(1).trim().split(/ +/).shift().toLowerCase()//Kalau mau Single prefix Lu ganti pake ini = const command = body.slice(1).trim().split(/ +/).shift().toLowerCase()
 const args = body.trim().split(/ +/).slice(1)
 const pushname = m.pushName || "No Name"
 const botNumber = await haikal.decodeJid(haikal.user.id)
